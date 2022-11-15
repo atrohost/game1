a7bf1c6948d5ec23b6c289cd4409487d813242ba
atrohost <118260866+atrohost@users.noreply.github.com>
2022-11-15 14:34:58 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1668502015,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/skin0-sheet0.png",
+		"images/rainbowcircle-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sound-sheet0.png",
+		"media/soundback.m4a",
+		"media/soundback.ogg"
+	]
+}
